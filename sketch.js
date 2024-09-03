d3d722bc13a83ebb711751d83e65c8c31f009946
Dr-pimpolho <00001125064341sp@al.educacao.sp.gov.br>
2024-09-03 09:34:37 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,141 @@
+function geraRecomendacao(idade, gostaDeFantasia) {
+    if (idade >= 10) {
+        if (idade >= 14) {
+            return "O menino que descobriu o vento";
+        } else {
+            if (gostaDeFantasia) {
+                return "As aventuras de pi";
+            } else {
+                return "Depois da chuva";
+            }
+        }
+    } else {
+        if (gostaDeFantasia) {
+            return "A viagem de chihiro";
+        } else {
+            return "O feitiço do tempo";
+        }
+    }
+}function geraRecomendacao(idade, gostaDeFantasia) {
+  if (idade >= 10) {
+    if (idade >= 14) {
+      return "O menino que descobriu o vento";
+    } else {
+      if (idade >= 12) {
+        return "Ladrões de bicicleta";
+      } else {
+        if (gostaDeFantasia) {
+          return "As aventuras de pi";
+        } else {
+          return "Depois da chuva";
+        }
+      }
+    }
+  } else {
+    if (gostaDeFantasia) {
+      return "A viagem de chihiro";
+    } else {
+      return "O feitiço do tempo";
+    }
+  }
+}function geraRecomendacao(idade, gostaDeFantasia) {
+  if(idade >= 10) {
+    if(idade >= 14) {
+      return "O menino que descobriu o vento";
+    } else {
+      if(idade >= 12) {
+        if(gostaDeFantasia) {
+          return "Homem-aranha: no aranhaverso";
+        } else {
+          return "Ladrões de bicicleta";
+        }
+      }
+    }
+  }
+}let campoIdade;
+let campoFantasia;
+let campoAventura;
+
+function setup() {
+  createCanvas(800, 400);
+  createElement("h2", "Recomendador de filmes");
+  createSpan("Sua idade:");
+  campoIdade = createInput("5");
+  campoFantasia = createCheckbox("Gosta de fantasia?");
+  campoAventura = createCheckbox("Gosta de aventura?");
+}function draw() {
+  background("white");
+  let idade = campoIdade.value();
+  let gostaDeFantasia = campoFantasia.checked();
+  let gostaDeAventura = campoAventura.checked();
+  let recomendacao = geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura);
+
+  fill(color(76, 0, 115));
+  textAlign(CENTER, CENTER);
+  textSize(38);
+  text(recomendacao, width / 2, height / 2);
+}function geraRecomendacao(idade, gostaDeFantasia) {
+  if (idade >= 10) {
+    if (idade >= 14) {
+      return "O menino que descobriu o vento";
+    } else {
+      if (idade >= 12) {
+        if(gostaDeFantasia) {
+          return "Homem aranha: no aranhaverso";          
+        } else{
+         return "Ladrões de bicicleta";
+        }
+      } else {
+        if (gostaDeFantasia) {
+          return "As aventuras de pi";
+        } else {
+          return "Depois da chuva";
+        }
+      }
+    }
+  } else {
+    if (gostaDeFantasia) {
+      return "A viagem de chihiro";
+    } else {
+      return "O feitiço do tempo";
+    }
+  }
+}function geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura) {
+  if (idade >= 10) {
+    if (idade >= 14) {
+      return "O menino que descobriu o vento";
+    } else {
+      if (idade >= 12) {
+        if(gostaDeFantasia || gostaDeAventura) {
+          return "Homem aranha: no aranhaverso";          
+        } else{
+         return "Ladrões de bicicleta";
+        }
+      } else {
+        if (gostaDeFantasia) {
+          return "As aventuras de pi";
+        } else {
+          return "Depois da chuva";
+        }
+      }
+    }
+  } else {
+    if (gostaDeFantasia) {
+      return "A viagem de chihiro";
+    } else {
+      return "O feitiço do tempo";
+    }
+  }
+let possuiPermissao = true;
+
+function verificaIdade(idade, possuiPermissao) {
+  if (idade >= 18 || possuiPermissao) {
+    return "Você pode dirigir!";
+  } else {
+    return "Você não pode dirigir.";
+  }
+}function verificaAcesso() {
+
+    return "Bem-vindo(a), professor(a)!";
+  }
+}
